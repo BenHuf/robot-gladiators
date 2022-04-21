c8222f69f22267c4272dbb1cb8764a4100a39c94
BenHuf <benthehuffman@gmail.com>
2022-04-21 15:33:28 -0400

    Added console.log tests

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! JavaScript is running!");
+// assigns playerName as whatever is entered into the prompt
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+// console.log tests
+console.log("Our robot's name is " + playerName);
+//this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
